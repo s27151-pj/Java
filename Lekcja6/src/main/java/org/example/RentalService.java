64c96d9de84fb84ce7588ead8ba563ab4e5b74ab
s27151-pj <115358243+s27151-pj@users.noreply.github.com>
2024-01-13 09:57:25 +0100

    Add files via upload

---
+++ Lekcja6/src/main/java/org/example/RentalService.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+public class RentalService {
+
+    private CarStorage carStorage;
+    private RentStorage rentStorage;
+
+    public RentalService(CarStorage carStorage, RentStorage rentStorage) {
+        this.carStorage = carStorage;
+        this.rentStorage = rentStorage;
+    }
+
+
+    int baseOneDayRent = 100;
+
+    public CarRentInfo createRent(LocalDate dateFrom, LocalDate dateTo, Car car, user user) {
+        if (!car.isRented() && dateFrom.isBefore(dateTo)) {
+            car.setRented(true);
+            car.setTemporaryOwner(user);
+            return new CarRentInfo(car.getTemporaryOwner(), car.getVin(), dateFrom, dateTo);
+        } else {
+            System.out.println("Błąd przy tworzeniu rezerwacji");
+        }
+        return null;
+    }
+
+    public boolean carExist(int vin) {
+        return carStorage.getCarList().stream().anyMatch(v -> v.getVin() == vin);
+    }
+
+    public double estimatePrice(int vin, LocalDate dateFrom, LocalDate dateTo) {
+        double price = baseOneDayRent;
+        double daysBetween = ChronoUnit.DAYS.between(dateFrom, dateTo);
+        Car carPrice = carStorage.getCarList().stream()
+                .filter(car -> car.getVin() == vin)
+                .findAny()
+                .orElseThrow();
+
+        price *= daysBetween * carPrice.getSegment().getStandardValue();
+        price = (double) Math.round(price * 100.0) / 100.0;
+
+        return price;
+    }
+}
